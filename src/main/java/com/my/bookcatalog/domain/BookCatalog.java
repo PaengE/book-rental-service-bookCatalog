f721645922a6b856f9e0a9b70da00e478c097da8
PaengE <ckddn2820@gmail.com>
2021-06-29 20:52:27 +0900

    fix: lombok issue

--- src/main/java/com/my/bookcatalog/domain/BookCatalog.java
+++ src/main/java/com/my/bookcatalog/domain/BookCatalog.java
@@ -2,6 +2,8 @@ package com.my.bookcatalog.domain;
 
 import java.io.Serializable;
 import java.time.LocalDate;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
@@ -9,6 +11,8 @@ import org.springframework.data.mongodb.core.mapping.Field;
 /**
  * A BookCatalog.
  */
+@Getter
+@Setter
 @Document(collection = "book_catalog")
 public class BookCatalog implements Serializable {
 
@@ -42,21 +46,9 @@ public class BookCatalog implements Serializable {
     private Long bookId;
 
     // jhipster-needle-entity-add-field - JHipster will add fields here
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
     public BookCatalog id(String id) {
         this.id = id;
         return this;
-    }
-
-    public String getTitle() {
-        return this.title;
     }
 
     public BookCatalog title(String title) {
@@ -64,25 +56,9 @@ public class BookCatalog implements Serializable {
         return this;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getAuthor() {
-        return this.author;
-    }
-
     public BookCatalog author(String author) {
         this.author = author;
         return this;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public String getDescription() {
-        return this.description;
     }
 
     public BookCatalog description(String description) {
@@ -90,25 +66,9 @@ public class BookCatalog implements Serializable {
         return this;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public LocalDate getPublicationDate() {
-        return this.publicationDate;
-    }
-
     public BookCatalog publicationDate(LocalDate publicationDate) {
         this.publicationDate = publicationDate;
         return this;
-    }
-
-    public void setPublicationDate(LocalDate publicationDate) {
-        this.publicationDate = publicationDate;
-    }
-
-    public String getClassification() {
-        return this.classification;
     }
 
     public BookCatalog classification(String classification) {
@@ -116,25 +76,9 @@ public class BookCatalog implements Serializable {
         return this;
     }
 
-    public void setClassification(String classification) {
-        this.classification = classification;
-    }
-
-    public Boolean getRented() {
-        return this.rented;
-    }
-
     public BookCatalog rented(Boolean rented) {
         this.rented = rented;
         return this;
-    }
-
-    public void setRented(Boolean rented) {
-        this.rented = rented;
-    }
-
-    public Long getRentCnt() {
-        return this.rentCnt;
     }
 
     public BookCatalog rentCnt(Long rentCnt) {
@@ -142,21 +86,9 @@ public class BookCatalog implements Serializable {
         return this;
     }
 
-    public void setRentCnt(Long rentCnt) {
-        this.rentCnt = rentCnt;
-    }
-
-    public Long getBookId() {
-        return this.bookId;
-    }
-
     public BookCatalog bookId(Long bookId) {
         this.bookId = bookId;
         return this;
-    }
-
-    public void setBookId(Long bookId) {
-        this.bookId = bookId;
     }
 
     // jhipster-needle-entity-add-getters-setters - JHipster will add getters and setters here
--- src/main/java/com/my/bookcatalog/web/rest/dto/BookCatalogDTO.java
+++ src/main/java/com/my/bookcatalog/web/rest/dto/BookCatalogDTO.java
@@ -3,10 +3,14 @@ package com.my.bookcatalog.web.rest.dto;
 import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.Objects;
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  * A DTO for the {@link com.my.bookcatalog.domain.BookCatalog} entity.
  */
+@Getter
+@Setter
 public class BookCatalogDTO implements Serializable {
 
     private String id;
@@ -26,78 +30,6 @@ public class BookCatalogDTO implements Serializable {
     private Long rentCnt;
 
     private Long bookId;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public LocalDate getPublicationDate() {
-        return publicationDate;
-    }
-
-    public void setPublicationDate(LocalDate publicationDate) {
-        this.publicationDate = publicationDate;
-    }
-
-    public String getClassification() {
-        return classification;
-    }
-
-    public void setClassification(String classification) {
-        this.classification = classification;
-    }
-
-    public Boolean getRented() {
-        return rented;
-    }
-
-    public void setRented(Boolean rented) {
-        this.rented = rented;
-    }
-
-    public Long getRentCnt() {
-        return rentCnt;
-    }
-
-    public void setRentCnt(Long rentCnt) {
-        this.rentCnt = rentCnt;
-    }
-
-    public Long getBookId() {
-        return bookId;
-    }
-
-    public void setBookId(Long bookId) {
-        this.bookId = bookId;
-    }
 
     @Override
     public boolean equals(Object o) {
